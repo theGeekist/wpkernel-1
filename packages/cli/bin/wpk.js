e967202c2b75e8c50e9c7e91bb36e8a83d99a258
Jason Joseph Nathan <780157+pipewrk@users.noreply.github.com>
2025-10-15 05:34:21 +0800

    Fix CLI init template resolution and composer scaffold

--- packages/cli/bin/wpk.js
+++ packages/cli/bin/wpk.js
@@ -6,7 +6,8 @@
  * Main executable entry point
  */
 
-globalThis.__WPK_CLI_MODULE_URL__ = import.meta.url;
+const initModuleUrl = new URL('../dist/commands/init.js', import.meta.url).href;
+globalThis.__WPK_CLI_MODULE_URL__ = initModuleUrl;
 
 try {
 	const { runCli } = await import('../dist/cli/run.js');
