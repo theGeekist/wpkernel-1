2b6a1808b83fb55cc93618517c4995929813351e
Jason Nathan <780157+jasonnathan@users.noreply.github.com>
2025-10-24 18:09:45 +0800

    fixed build

--- packages/cli/src/next/builders/php/resource/wpPost/list.ts
+++ packages/cli/src/next/builders/php/resource/wpPost/list.ts
@@ -19,7 +19,7 @@ import {
 	buildBooleanNot,
 	buildInstanceof,
 	buildPropertyFetch,
-	buildArrayInitialiserStatement,
+	// buildArrayInitialiserStatement,
 } from '../utils';
 
 export function buildListItemsInitialiserStatement(): PhpStmt {
