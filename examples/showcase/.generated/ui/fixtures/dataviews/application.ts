50018966ca23358e02d51cd643f18f9912ffaed7
Jason Nathan <780157+jasonnathan@users.noreply.github.com>
2025-11-18 10:02:34 +0800

    deleted showcase artifacts for a fresh start

--- examples/showcase/.generated/ui/fixtures/dataviews/application.ts
+++
@@ -1,7 +0,0 @@
-import { type ResourceDataViewConfig } from '@wpkernel/ui/dataviews';
-import * as wpkConfigModule from '../../../../wpk.config';
-
-export const applicationDataViewConfig: ResourceDataViewConfig<
-	unknown,
-	unknown
-> = wpkConfigModule.wpkConfig.resources['application'].ui!.admin!.dataviews;
